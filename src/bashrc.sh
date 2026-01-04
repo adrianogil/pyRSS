6a99f9f1a7910e86a46e07847a4915c0130c1a9a
Adriano Gil <adrianogil.san@gmail.com>
2026-01-04 09:54:33 -0400

    feat: add environment variable setup for PYRSS_PYTHON_PATH

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -1,3 +1,9 @@
+if [ -z "$PYRSS_PYTHON_PATH" ]
+then
+    export PYRSS_PYTHON_PATH=$PYRSS_DIR/
+    export PYTHONPATH=$PYRSS_PYTHON_PATH:$PYTHONPATH
+fi
+
 
 alias pyrss="python3 ${PYRSS_DIR}/pyrss.py"
 
